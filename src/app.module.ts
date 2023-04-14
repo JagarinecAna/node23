7448b173e1af785936f5ff5cd71597d00a9a506b
Hadis <hadiskurtisi75@gmail.com>
2023-04-14 10:30:25 +0200

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true
+        }),
+        UserModule,
+        DatabaseModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: parseInt(process.env.DATABASE_PORT, 10),
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD,
+            database: process.env.DATABASE_NAME,
+            autoLoadEntities:true,
+            entities:[],
+            synchronize: true,
+        }),
+
+        UserModule,
+        DatabaseModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
